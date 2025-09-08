19c31930157337ba9c74cc8e73355c4c6ff89e90
Shurjeel <shurjeel_m@hotmail.com>
2025-09-08 20:20:04 +0100

    can change color of div upon hover. can change the number of squares of div.

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 const container = document.querySelector(`.container`);
-for (let i = 0; i <256; i++) {
-    const div = document.createElement(`div`);
-    div.classList.toggle("box");
-    div.setAttribute("style", "padding: 0px; margin: 0; border: 2px solid black; width: 30px; height: 30px;");
-    container.appendChild(div);
+// for (let i = 0; i <256; i++) {
+//     const div = document.createElement(`div`);
+//     div.classList.toggle("box");
+//     div.setAttribute("style", "padding: 0px; margin: 0; border: 2px solid black; width: 30px; height: 30px;");
+//     container.appendChild(div);
+// }
+createGrid(16);
+color();
+
+
+
+const gridBtn = document.querySelector(`#gridBtn`);
+
+gridBtn.addEventListener("click", () => {
+    let squares = prompt(`How many squares per side?`);
+    const boxes = document.querySelectorAll(".box");
+    container.innerHTML="";
+    createGrid(squares);
+    
+});
+
+
+
+function createGrid(squares) {
+    const number = squares * squares;
+    const width = 480 / squares;
+    for (let i = 0; i <number; i++) {
+        const div = document.createElement(`div`);
+        div.classList.toggle("box");
+        div.setAttribute("style", "padding: 0px; margin: 0; border: 2px solid black; width: " + width + "px; height: " + width + "px;");
+        container.appendChild(div);
+    }
+    color();
 }
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        alert("hey there");
+function color() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "red"; 
+        });
     });
-});
+}
